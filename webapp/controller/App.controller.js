f1f3aef8a31c48de28af3fd34610284b573b9767
sc4mp <58878850+scaamp@users.noreply.github.com>
2023-08-05 19:58:44 +0200

    Button added

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,23 +1,34 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
- ], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-       onInit : function () {
-          // set data model on view
-          var oData = {
-             recipient : {
-                name : "Jakub"
-             }
-          };
-          var oModel = new JSONModel(oData);
-          this.getView().setModel(oModel);
-       },
-       onShowHello : function () {
-        var oInput = this.byId("input1");
-          MessageToast.show(oInput.getDescription());
-       }
+        onInit: function () {
+            // set data model on view
+            var oData = {
+                recipient: {
+                    name: "Jakub"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+            var i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+             });
+             this.getView().setModel(i18nModel, "i18n");
+        },
+        onShowHello: function () {
+            // var oInput = this.byId("input1");
+            // MessageToast.show(oInput.getDescription());
+
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            MessageToast.show(sMsg);
+
+        }
     });
- });
+});
